5b664cc5541076ed6f4c59d4c320706ac239d590
Talha <cooltalha27@gmail.com>
2021-04-22 11:58:26 +0530

    Completed

---
+++ app.py
@@ -0,0 +1,10 @@
+import qrcode
+input_data = "https://github.com/Talha-Altair?tab=repositories"
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode001.png')
